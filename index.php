150487165586cc86186bf268377d7a8dc399ab14
erickok <erickok@57d32664-b73f-11de-b129-73e2b2a9d7e9>
2009-10-20 10:11:36 +0000

    Extracted header and footer HTML to seperate files for easy customization.
    git-svn-id: https://android-php-translator.googlecode.com/svn/trunk@4 57d32664-b73f-11de-b129-73e2b2a9d7e9

--- index.php
+++ index.php
@@ -2,22 +2,8 @@
 
 	include('includes/common.php');
 	
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title><?php echo $appname; ?></title>
-	<style type="text/css">
-		@import url("includes/default.css");
-	</style>
-</head>
-
-<body>
-
-<?php
+	$pageTitle = $appname;
+	include('includes/header.php');
 
 	echo '
 	<h1>' . $appname . '</h1>';
@@ -57,8 +43,8 @@
 			</td>
 		</tr>
 	</table>';
-?>
+	
+	include('includes/footer.php');
 
-</body>
-</html>
+?>
 
--- translation.php
+++ translation.php
@@ -65,22 +65,7 @@
 		
 	}
 	
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Edit a translation - <?php echo $appname; ?></title>
-	<style type="text/css">
-		@import url("includes/default.css");
-	</style>
-</head>
-
-<body>
-
-<?php
+	$pageTitle = 'Edit a translation - ' . $appname;
 
 	echo '
 	<h1>Translating to ' . $iso639[$lang] . ' (' . $lang . ')</h1>';
@@ -135,9 +120,8 @@
 		</tr>
 	</table>
 	</form>';
+	
+	include('includes/footer.php');
 
 ?>
-
-</body>
-</html>
 
